545358c3a060bc6d4724f437a7c35dea74367bc0
fjosne <simen@fjosne.net>
2021-10-21 13:05:20 +0200

    add new keybinds

--- config.def.h
+++ config.def.h
@@ -62,6 +62,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *browsercmd[]  = { "qutebrowser", NULL };
 
 /* volume control */
 static const char *upvol[]   = { "/usr/bin/pulseaudio-ctl", "up", "5", NULL};
@@ -72,7 +73,7 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ 0,                            XK_F12,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
@@ -107,7 +108,7 @@ static Key keys[] = {
     { Mod4Mask,                     XK_k,      spawn,          {.v = upvol } },
     { Mod4Mask,                     XK_j,      spawn,          {.v = downvol } },
     { Mod4Mask,                     XK_m,      spawn,          {.v = mutevol } },
-
+    { MODKEY,                       XK_b,      spawn,          {.v = browsercmd } },
 };
 
 /* button definitions */
